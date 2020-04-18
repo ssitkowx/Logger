7bcb0a67332c7fac7e2ddbf64ca074314025f0c0
sylsit <sitkowskis@interia.pl>
2020-04-18 15:22:51 +0200

    conan paths update

--- Conan/conanfile.py
+++ Conan/conanfile.py
@@ -43,19 +43,27 @@ class Conan(ConanFile):
             self.createPackage  (user,channel)
 
     def build(self):
-        projectPath = self.downloadsPath + '\\' + self.name
+        projectPath  = os.getcwd().replace('\Conan','')
+        projectBuild = projectPath + '\\Build'
         
+        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+            projectPath  = self.downloadsPath + '\\' + self.name
+            projectBuild = os.getcwd() + '\\Build'
+
         if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
             cmake = CMake(self)
-            cmake.configure(source_dir=projectPath)
+            cmake.configure(source_dir=projectPath, build_dir=projectBuild)
             cmake.build()
         else:
             raise Exception('Unsupported platform or compiler')
             
         tools.replace_in_file(projectPath + "\\CMakeLists.txt", "Template", self.name, False)
         
-    def package(self):     
-        projectPath = self.downloadsPath + '\\' + self.name
+    def package(self):   
+        projectPath = os.getcwd().replace('\Conan','')
+        
+        if not os.path.exists(projectPath + '\\CMakeLists.txt'):
+            projectPath = self.downloadsPath + '\\' + self.name
     
         self.copy('*.h'     , dst='include', src= projectPath + '\\Project' , keep_path=False)
         self.copy('*.hxx'   , dst='include', src= projectPath + '\\Project' , keep_path=False)
@@ -64,6 +72,4 @@ class Conan(ConanFile):
         self.copy('*.dylib*', dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
         self.copy('*.so'    , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
         self.copy('*.a'     , dst='lib'    , src= projectPath + '\Build\lib', keep_path=False)
-
-    def package_info(self):
-        self.cpp_info.libs = [self.name]
+        
