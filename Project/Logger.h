3dea085e5322588bb1065dc9e24e8333fa6d88c1
sylsit <sitkowskis@interia.pl>
2021-01-06 11:48:19 +0100

    optimization with string_view

--- Project/Logger.h
+++ Project/Logger.h
@@ -27,12 +27,12 @@ class Logger
         static Logger * GetInstance (void) { return NULL; }
 
         template <class DERIVED_TYPE, typename... ARGS>
-        void Log (const ELogLevel      v_eLogLevel,
-                  const std::string && v_module,
-                  const std::string && v_msg,
-                  ARGS &&...           v_args)
+        constexpr void Log (const ELogLevel  v_eLogLevel,
+                            std::string_view v_module,
+                            std::string_view v_msg,
+                            ARGS &&...       v_args)
         {
-            static_cast <DERIVED_TYPE &>(* this).Log (v_eLogLevel, std::move (v_module), std::move (v_msg), v_args...);
+            static_cast <DERIVED_TYPE &>(* this).Log (v_eLogLevel, v_module, v_msg, v_args...);
         }
 };
 
